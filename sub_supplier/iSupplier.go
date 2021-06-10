3219e05626ecb6b6600094a2ed42c684d322541c
allan716 <525223688@qq.com>
2021-06-10 17:41:41 +0800

    更新 TODO 备注
    Signed-off-by: allan716 <525223688@qq.com>

--- sub_supplier/iSupplier.go
+++ sub_supplier/iSupplier.go
@@ -1,7 +1,10 @@
 package sub_supplier
 
 type ISupplier interface {
-
+	// TODO 这里需要考虑是什么类型的视频文件，可能是 电影、连续剧、动画，这三类应该有细节上的差异
+	// 比如，连续剧，有按季下载整个季字幕包，也可能是每一季的每一集下载一个
+	// 电影则可以直接用 IMDB 直接下载或者削刮后的文件名去下载
+	// 动画，嗯···还没啥经验，额外粗略看来，很多坑
 	GetSupplierName() string
 
 	GetSubListFromFile(filePath string) ([]SubInfo, error)
