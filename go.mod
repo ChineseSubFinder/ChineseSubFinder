c690d3b1621df8333a05dee3e13a54ac6f6c0e8f
716 <525223688@qq.com>
2021-06-10 22:10:28 +0800

    完成 srt 字幕的检测
    Signed-off-by: 716 <525223688@qq.com>

--- go.mod
+++ go.mod
@@ -4,15 +4,16 @@ go 1.15
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.1
-	github.com/abadojack/whatlanggo v1.0.1 // indirect
+	github.com/abadojack/whatlanggo v1.0.1
 	github.com/beevik/etree v1.1.0
 	github.com/go-resty/resty/v2 v2.6.0
 	github.com/go-rod/rod v0.97.2
-	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/lestrrat-go/strftime v1.0.4 // indirect
 	github.com/middelink/go-parse-torrent-name v0.0.0-20190301154245-3ff4efacd4c4
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816 // indirect
+	github.com/sirupsen/logrus v1.8.1
+	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816
 )
--- sub_parser/srt/srt.go
+++ sub_parser/srt/srt.go
@@ -41,10 +41,43 @@ func (a Parser) DetermineFileType(filePath string) (*sub_parser.SubFileInfo, err
 	subFileInfo.Dialogues = make([]sub_parser.OneDialogue, 0)
 	// 这里需要统计一共有几个 \N，以及这个数量在整体行数中的比例，这样就知道是不是双语字幕了
 	countLineFeed := 0
-
-	println(countLineFeed)
-	return nil, nil
+	for _, oneDial := range matched {
+		startTime := oneDial[2]
+		endTime := oneDial[3]
+		nowText := oneDial[4]
+		odl := sub_parser.OneDialogue{
+			StartTime: startTime,
+			EndTime: endTime,
+		}
+		odl.Lines = make([]string, 0)
+		nowText = strings.TrimRight(nowText, "\n")
+		texts := strings.Split(nowText, "\n")
+		for i, text := range texts {
+			if i == 1 {
+				// 这样说明有两行字幕，也就是双语啦
+				countLineFeed++
+			}
+			odl.Lines = append(odl.Lines, text)
+		}
+		subFileInfo.Dialogues = append(subFileInfo.Dialogues, odl)
+	}
+	// 再分析
+	// 是不是双语字幕，定义，超过 80% 就一定是了（不可能三语吧···）
+	isDouble := false
+	perLines := float32(countLineFeed) / float32(len(matched))
+	if perLines > 0.8 {
+		isDouble = true
+	}
+	// 需要判断每一个 Line 是啥语言，[语言的code]次数
+	var langDict map[int]int
+	langDict = make(map[int]int)
+	for _, dialogue := range subFileInfo.Dialogues {
+		common.DetectSubLangAndStatistics(dialogue.Lines, langDict)
+	}
+	// 从统计出来的字典，找出 Top 1 或者 2 的出来，然后计算出是什么语言的字幕
+	detectLang := common.SubLangStatistics2SubLangType(isDouble, langDict)
+	subFileInfo.Lang = detectLang
+	return &subFileInfo, nil
 }
 
-//const regString = `(?i)(\d+)\s+([\d:,]+)\s+-{2}\>\s+([\d:,]+)\s+([\s\S]*?(\s{2}|$))`
 const regString = `(\d+)\n([\d:,]+)\s+-{2}\>\s+([\d:,]+)\n([\s\S]*?(\n{2}|$))`
