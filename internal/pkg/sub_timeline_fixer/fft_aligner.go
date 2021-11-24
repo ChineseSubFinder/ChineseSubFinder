606f989cbe0dfd9fcd586b449704c390720578ec
allan716 <525223688@qq.com>
2021-11-24 14:30:45 +0800

    完成，复现基础的 ffsubsync 的 FFTAligner 算法
    Signed-off-by: allan716 <525223688@qq.com>

--- internal/pkg/sub_timeline_fixer/fft_aligner.go
+++ internal/pkg/sub_timeline_fixer/fft_aligner.go
@@ -1,6 +1,8 @@
 package sub_timeline_fixer
 
 import (
+	"github.com/emirpasic/gods/maps/treemap"
+	"github.com/emirpasic/gods/utils"
 	"gonum.org/v1/gonum/cmplxs"
 	"gonum.org/v1/gonum/dsp/fourier"
 	"gonum.org/v1/gonum/floats"
@@ -8,32 +10,42 @@ import (
 )
 
 /*
-	复现 https://github.com/smacke/ffsubsync 的算法
+	复现 https://github.com/smacke/ffsubsync 的 FFTAligner 算法
 */
 type FFTAligner struct {
 }
 
-func (f FFTAligner) fit(refFloats, subFloats []float64) {
+func NewFFTAligner() *FFTAligner {
+	return &FFTAligner{}
+}
+
+// Fit 给出最佳的偏移，还需要根据实际情况进行转换（比如，1 步 是 10 ms）
+func (f FFTAligner) Fit(refFloats, subFloats []float64) (int, float64) {
+	return f.computeArgmax(f.fit(refFloats, subFloats), subFloats)
+}
+
+// fit 返回 convolve
+func (f FFTAligner) fit(refFloats, subFloats []float64) []float64 {
 
 	// 先初始化一个 fft 共用实例
 	fftIns := fourier.NewFFT(1000)
 	// 计算出一维矩阵的长度
-	total_bits := math.Log2(float64(len(refFloats)) + float64(len(subFloats)))
-	total_length := int(math.Pow(2, math.Ceil(total_bits)))
+	totalBits := math.Log2(float64(len(refFloats)) + float64(len(subFloats)))
+	totalLength := int(math.Pow(2, math.Ceil(totalBits)))
 	// 需要补零的个数
-	extra_zeros := total_length - len(refFloats) - len(subFloats)
+	extraZeros := totalLength - len(refFloats) - len(subFloats)
 	// 2 的倍数长度
-	power2Len := extra_zeros + len(refFloats) + len(subFloats)
+	power2Len := extraZeros + len(refFloats) + len(subFloats)
 	// ----------------------------------------------------------
 	// 对于 sub 需要在前面补零
 	power2Sub := make([]float64, power2Len)
-	for i := 0; i < extra_zeros+len(refFloats); i++ {
+	for i := 0; i < extraZeros+len(refFloats); i++ {
 		power2Sub[i] = 0
 	}
 	for i := 0; i < len(subFloats); i++ {
-		power2Sub[extra_zeros+len(subFloats)+i] = subFloats[i]
+		power2Sub[extraZeros+len(subFloats)+i] = subFloats[i]
 	}
-	// "github.com/brettbuddin/fourier"
+	// 可选择的 FFT 实现 "github.com/brettbuddin/fourier"
 	//subFT := fourier.Forward()
 	fftIns.Reset(len(power2Sub))
 	subFT := fftIns.Coefficients(nil, power2Sub)
@@ -43,7 +55,7 @@ func (f FFTAligner) fit(refFloats, subFloats []float64) {
 	for i := 0; i < len(refFloats); i++ {
 		power2Ref[i] = refFloats[i]
 	}
-	for i := 0; i < extra_zeros+len(subFloats); i++ {
+	for i := 0; i < extraZeros+len(subFloats); i++ {
 		power2Ref[len(refFloats)+i] = 0
 	}
 	// 反转 power2Ref  0, 1，1，0，0 -> 0,0,1,1,0
@@ -56,15 +68,22 @@ func (f FFTAligner) fit(refFloats, subFloats []float64) {
 	// 先计算 subFT * refFT，结果放置在 refFT
 	cmplxs.Mul(refFT, subFT)
 	// 然后执行 numpy 的 ifft 操作
-	gotRefFT := fftIns.Sequence(nil, refFT)
-	floats.Scale(1/float64(len(power2Ref)), gotRefFT)
+	convolve := fftIns.Sequence(nil, refFT)
+	floats.Scale(1/float64(len(power2Ref)), convolve)
 
-	//refFloatsVec := mat.NewVecDense(len(refFloats), refFloats)
-	//subFloatsVec := mat.NewVecDense(len(subFloats), subFloats)
-	println("d")
-	//a := mat.NewVecDense(extra_zeros+refFloatsVec.Len(), nil)
+	return convolve
 }
 
-func float642comolex() {
+// computeArgmax 找对最优偏移，还需要根据实际情况进行转换（比如，1 步 是 10 ms）
+func (f FFTAligner) computeArgmax(convolve, subFloats []float64) (int, float64) {
 
+	convolveTM := treemap.NewWith(utils.Float64Comparator)
+	for i, value := range convolve {
+		convolveTM.Put(value, i)
+	}
+	bestScore, bestIndex := convolveTM.Max()
+
+	bestOffset := len(convolve) - 1 - bestIndex.(int) - len(subFloats)
+
+	return bestOffset, bestScore.(float64)
 }
--- internal/pkg/sub_timeline_fixer/fft_aligner_test.go
+++ internal/pkg/sub_timeline_fixer/fft_aligner_test.go
@@ -1,25 +1,37 @@
 package sub_timeline_fixer
 
-import "testing"
+import (
+	"gonum.org/v1/gonum/floats/scalar"
+	"testing"
+)
 
-func TestFFTAligner_fit(t *testing.T) {
+func TestFFTAligner_Fit(t *testing.T) {
 	type args struct {
 		refFloats []float64
 		subFloats []float64
 	}
 	tests := []struct {
-		name string
-		args args
+		name  string
+		args  args
+		want  int
+		want1 float64
 	}{
 		{name: "00", args: args{
-			refFloats: []float64{1, 1, 1, 1, -1, -1, 1},
-			subFloats: []float64{-1, 1, 1, -1, -1, 1, -1},
-		}},
+			refFloats: []float64{1, 1, 1, 1, 0, 0, 1},
+			subFloats: []float64{0, 1, 1, 0, 0, 1, 0},
+		}, want: 1, want1: 5},
 	}
+	const tol = 1e-10
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			f := FFTAligner{}
-			f.fit(tt.args.refFloats, tt.args.subFloats)
+			got, got1 := f.Fit(tt.args.refFloats, tt.args.subFloats)
+			if got != tt.want {
+				t.Errorf("Fit() got = %v, want %v", got, tt.want)
+			}
+			if scalar.EqualWithinAbsOrRel(got1, tt.want1, tol, tol) {
+				t.Errorf("Fit() got1 = %v, want %v", got1, tt.want1)
+			}
 		})
 	}
 }
