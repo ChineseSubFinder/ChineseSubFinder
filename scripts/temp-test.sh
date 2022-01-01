c312f82212033a41fe6fa775b638c3098dd2b5e6
sdsadfaf <wangyq977@gmail.com>
2022-01-01 19:43:57 +0100

    fix: run more internal/pkg unit test in temp-test.sh
    * now still have failed tests but files location are correct
    * tests related to external video/audio files are commented

--- scripts/temp-test.sh
+++ scripts/temp-test.sh
@@ -3,11 +3,12 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. 
 echo "==> Running selective tests..."
 go test ./internal/pkg/archive_helper/
-# go test ./internal/pkg/calculate_curve_correlation # fail
+go test ./internal/pkg/calculate_curve_correlation
 # go test ./internal/pkg/config
 # go test ./internal/pkg/debug_view
 go test ./internal/pkg/decode
 go test ./internal/pkg/dtw
+echo "ERROR:  Emby server needed to run emby_api test, skipping.............."
 # go test ./internal/pkg/emby_api # fail
 # go test ./internal/pkg/ffmpeg_helper # fail
 # go test ./internal/pkg/frechet 
@@ -17,15 +18,19 @@ go test ./internal/pkg/gss
 go test ./internal/pkg/imdb_helper
 # go test ./internal/pkg/language
 # go test ./internal/pkg/log_helper
-go test ./internal/pkg/my_util # will produce Log dir
+# go test ./internal/pkg/my_util # will produce Log dir
+echo "ERROR:  Proxy needed for proxy_helper, skipping.............."
 # go test ./internal/pkg/proxy_helper # fail
 # go test ./internal/pkg/random_useragent
 # go test ./internal/pkg/regex_things
+echo "ERROR:  Proxy needed for rod_helper,  skipping.............."
 # go test ./internal/pkg/rod_helper # fail
 # go test ./internal/pkg/sqlite
-# go test ./internal/pkg/sub_formatter
-# go test ./internal/pkg/sub_helper
-# go test ./internal/pkg/sub_parser_hub
+# go test ./internal/pkg/sub_formatter # fail
+echo "ERROR:  srt and ass has differet len of dialogue sub_helper_test.go"
+go test ./internal/pkg/sub_helper
+go test ./internal/pkg/sub_parser_hub
+# fixer_test needs better files and audio
 # go test ./internal/pkg/sub_timeline_fixer # fail
 go test ./internal/pkg/vad
 # go test ./internal/pkg/vosk_api
