74b7ec487f066332eb08f5554ff49b9eb0a4ad1a
allan716 <525223688@qq.com>
2022-01-07 14:19:30 +0800

    修复测试用例
    Signed-off-by: allan716 <525223688@qq.com>

--- internal/pkg/notify_center/NotifyCenter_test.go
+++ internal/pkg/notify_center/NotifyCenter_test.go
@@ -1,14 +1,12 @@
 package notify_center
 
 import (
-	"github.com/allanpk716/ChineseSubFinder/internal/pkg/config"
 	"testing"
 )
 
 func TestNewNotifyCenter(t *testing.T) {
 
-	config := config.GetConfig()
-	center := NewNotifyCenter(config.WhenSubSupplierInvalidWebHook)
+	center := NewNotifyCenter("https://www.baidu.com/")
 	center.Add("groupName", "Info asd 哈哈")
 	center.Send()
 }
