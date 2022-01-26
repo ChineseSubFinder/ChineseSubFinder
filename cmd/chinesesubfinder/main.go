e96d15c357226bb89b93b4972009182597344150
allan716 <525223688@qq.com>
2022-01-26 12:38:02 +0800

    调整细节
    Signed-off-by: allan716 <525223688@qq.com>

--- cmd/chinesesubfinder/main.go
+++ cmd/chinesesubfinder/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/allanpk716/ChineseSubFinder/internal/backend"
 	"github.com/allanpk716/ChineseSubFinder/internal/logic/cron_helper"
 	"github.com/allanpk716/ChineseSubFinder/internal/pkg/global_value"
@@ -31,15 +32,19 @@ func main() {
 
 	if settings.GetSettings().UserInfo.Username == "" || settings.GetSettings().UserInfo.Password == "" {
 		// 如果没有完成，那么就不开启
-
+		log_helper.GetLogger().Infoln("Need do Setup")
 	} else {
 		// 是否完成了 Setup，如果完成了，那么就开启第一次的扫描
 		go func() {
 			cronHelper.Start(settings.GetSettings().CommonSettings.RunScanAtStartUp)
 		}()
 	}
+
+	nowPort := readCustomPortFile()
+	log_helper.GetLogger().Infoln(fmt.Sprintf("WebUI will listen at 0.0.0.0:%d", nowPort))
+
 	// 支持在外部配置特殊的端口号，以防止本地本占用了无法使用
-	backend.StartBackEnd(readCustomPortFile(), cronHelper)
+	backend.StartBackEnd(nowPort, cronHelper)
 }
 
 func readCustomPortFile() int {
