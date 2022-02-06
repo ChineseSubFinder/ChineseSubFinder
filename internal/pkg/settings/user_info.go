d10a0c8c95a041356c29656f62e24e297200f561
716 <525223688@qq.com>
2022-02-06 22:04:33 +0800

    调整后端用户密码限制最大长度为 30 位
    Signed-off-by: 716 <525223688@qq.com>

--- internal/pkg/settings/user_info.go
+++ internal/pkg/settings/user_info.go
@@ -2,7 +2,7 @@ package settings
 
 type UserInfo struct {
 	Username string `json:"username" binding:"required,alphanum"`     // 用户名
-	Password string `json:"password" binding:"required,min=6,max=12"` // 密码
+	Password string `json:"password" binding:"required,min=6,max=30"` // 密码
 }
 
 func NewUserInfo(userName, password string) *UserInfo {
