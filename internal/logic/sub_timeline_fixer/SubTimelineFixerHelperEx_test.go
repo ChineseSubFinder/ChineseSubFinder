81cc2019d70309a637da7e4660ff5135d31b37e2
allan716 <525223688@qq.com>
2022-02-10 08:59:50 +0800

    测试的数据超过上传的体积，删除这个部分
    Signed-off-by: allan716 <525223688@qq.com>

--- internal/logic/sub_timeline_fixer/SubTimelineFixerHelperEx_test.go
+++ internal/logic/sub_timeline_fixer/SubTimelineFixerHelperEx_test.go
@@ -1,9 +1,6 @@
 package sub_timeline_fixer
 
 import (
-	"github.com/allanpk716/ChineseSubFinder/internal/pkg/settings"
-	"github.com/allanpk716/ChineseSubFinder/internal/pkg/unit_test_helper"
-	"path/filepath"
 	"testing"
 )
 
@@ -18,32 +15,32 @@ func TestSubTimelineFixerHelperEx_Check(t *testing.T) {
 // TODO 暂不方便在其他环境进行单元测试
 func TestSubTimelineFixerHelperEx_Process(t *testing.T) {
 
-	rootDir := unit_test_helper.GetTestDataResourceRootPath([]string{"sub_timeline_fixer"}, 4, true)
-	type args struct {
-		videoFileFullPath string
-		srcSubFPath       string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "Foundation (2021) - S01E09", args: args{
-				videoFileFullPath: "X:\\连续剧\\Foundation (2021)\\Season 1\\Foundation (2021) - S01E09 - The First Crisis WEBDL-1080p.mkv",
-				srcSubFPath:       filepath.Join(rootDir, "series", "Foundation (2021)", "Season 1", "Foundation (2021) - S01E09 - The First Crisis WEBDL-1080p.chinese(简英,zimuku).ass")},
-			wantErr: false,
-		},
-	}
-
-	s := NewSubTimelineFixerHelperEx(*settings.NewTimelineFixerSettings())
-	s.Check()
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-
-			if err := s.Process(tt.args.videoFileFullPath, tt.args.srcSubFPath); (err != nil) != tt.wantErr {
-				t.Errorf("Process() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
+	//rootDir := unit_test_helper.GetTestDataResourceRootPath([]string{"sub_timeline_fixer"}, 4, true)
+	//type args struct {
+	//	videoFileFullPath string
+	//	srcSubFPath       string
+	//}
+	//tests := []struct {
+	//	name    string
+	//	args    args
+	//	wantErr bool
+	//}{
+	//	{
+	//		name: "Foundation (2021) - S01E09", args: args{
+	//			videoFileFullPath: "X:\\连续剧\\Foundation (2021)\\Season 1\\Foundation (2021) - S01E09 - The First Crisis WEBDL-1080p.mkv",
+	//			srcSubFPath:       filepath.Join(rootDir, "series", "Foundation (2021)", "Season 1", "Foundation (2021) - S01E09 - The First Crisis WEBDL-1080p.chinese(简英,zimuku).ass")},
+	//		wantErr: false,
+	//	},
+	//}
+	//
+	//s := NewSubTimelineFixerHelperEx(*settings.NewTimelineFixerSettings())
+	//s.Check()
+	//for _, tt := range tests {
+	//	t.Run(tt.name, func(t *testing.T) {
+	//
+	//		if err := s.Process(tt.args.videoFileFullPath, tt.args.srcSubFPath); (err != nil) != tt.wantErr {
+	//			t.Errorf("Process() error = %v, wantErr %v", err, tt.wantErr)
+	//		}
+	//	})
+	//}
 }
