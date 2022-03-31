b715ad1da7e27840ee1e97c915421a0e3ae67599
allan716 <525223688@qq.com>
2022-03-31 16:51:09 +0800

    简繁互相转换代码的测试
    Signed-off-by: allan716 <525223688@qq.com>

--- TestCode/test_chs_cht_convert.go
+++ TestCode/test_chs_cht_convert.go
@@ -8,13 +8,22 @@ func convertChsCht() {
 		dicter.ReadReverse 转繁体
 	*/
 	dicter := sat.DefaultDict()
+	println("---------------------")
 	sstr := "什麼"
+	println(sstr)
+	// 转换到 简体
 	println(dicter.Read(sstr))
+	// 转换到 繁体
 	println(dicter.ReadReverse(sstr))
+	println("---------------------")
 	sstr = "什么"
+	println(sstr)
 	println(dicter.Read(sstr))
 	println(dicter.ReadReverse(sstr))
+	println("---------------------")
 	sstr = "簡繁轉換"
+	println(sstr)
 	println(dicter.Read(sstr))
 	println(dicter.ReadReverse(sstr))
+	println("---------------------")
 }
