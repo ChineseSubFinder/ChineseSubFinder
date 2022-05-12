3d7eea50555dd464a6156922edaf014dd3d05570
allan716 <525223688@qq.com>
2022-05-12 18:02:32 +0800

    修复 api token 为空时候的反馈
    Signed-off-by: allan716 <525223688@qq.com>

--- internal/backend/middle/auth.go
+++ internal/backend/middle/auth.go
@@ -38,7 +38,11 @@ func CheckApiAuth() gin.HandlerFunc {
 			return
 		}
 		nowAccessToken := strings.Fields(authHeader)[1]
-		if nowAccessToken == "" || nowAccessToken != common.GetApiToken() {
+		if nowAccessToken == "" {
+			context.JSON(http.StatusUnauthorized, backend.ReplyCheckAuth{Message: "api_key_enabled == false or api_key is empty"})
+			context.Abort()
+			return
+		} else if nowAccessToken != common.GetApiToken() {
 			context.JSON(http.StatusUnauthorized, backend.ReplyCheckAuth{Message: "AccessToken Error"})
 			context.Abort()
 			return
