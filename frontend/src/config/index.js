63e041225588491456a50513e49052d4415ca42c
Myon <myon.cn@gmail.com>
2022-07-12 15:44:41 +0800

    feat(frontend): 后台服务19037合并到19035后的对接
    Signed-off-by: Myon <myon.cn@gmail.com>

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -2,8 +2,4 @@ export default {
   get BACKEND_URL() {
     return process.env.BACKEND_URL || new URL(window.location.href).pathname.replace(/\/$/, '');
   },
-
-  // 19037为静态服务默认端口
-  BACKEND_STATIC_URL:
-    process.env.BACKEND_STATIC_URL || `${window.location.protocol}//${window.location.hostname}:19037`,
 };
