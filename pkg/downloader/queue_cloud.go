8ac6d423a3147b62853e58a07f923b46e0f4171b
allan716 <525223688@qq.com>
2022-07-29 14:52:16 +0800

    还是考虑放弃在客户端集成分布式爬虫的功能，解决反爬比较麻烦
    Signed-off-by: allan716 <525223688@qq.com>

--- pkg/downloader/queue_cloud.go
+++ pkg/downloader/queue_cloud.go
@@ -1,7 +1,6 @@
 package downloader
 
 import (
-	"github.com/allanpk716/ChineseSubFinder/internal/dao"
 	"github.com/allanpk716/ChineseSubFinder/pkg/global_value"
 )
 
@@ -16,24 +15,24 @@ func (d *Downloader) queueDownloaderCloud() {
 	}
 
 	// 查询云端是否有任务
-	nowInfo := dao.UpdateInfo(global_value.AppVersion(), d.settings)
-	askDownloadTaskReply, err := d.fileDownloader.SubtitleBestApi.AskDownloadTask(nowInfo.Id)
-	if err != nil {
-		d.log.Errorf("queueDownloaderCloud AskDownloadTask error: %v", err)
-		return
-	}
-	if askDownloadTaskReply.Status == 0 {
-		// 失败
-		if askDownloadTaskReply.Message == "app version is too low" {
-			// 版本过低，不能获取任务
-			d.needSkipCloudTask = true
-			d.log.Warnf("queueDownloaderCloud AskDownloadTask error: %v", askDownloadTaskReply.Message)
-			return
-		}
-	} else if askDownloadTaskReply.Status == 2 {
-		// 没有任务
-		return
-	}
+	//nowInfo := dao.UpdateInfo(global_value.AppVersion(), d.settings)
+	//askDownloadTaskReply, err := d.fileDownloader.SubtitleBestApi.AskDownloadTask(nowInfo.Id)
+	//if err != nil {
+	//	d.log.Errorf("queueDownloaderCloud AskDownloadTask error: %v", err)
+	//	return
+	//}
+	//if askDownloadTaskReply.Status == 0 {
+	//	// 失败
+	//	if askDownloadTaskReply.Message == "app version is too low" {
+	//		// 版本过低，不能获取任务
+	//		d.needSkipCloudTask = true
+	//		d.log.Warnf("queueDownloaderCloud AskDownloadTask error: %v", askDownloadTaskReply.Message)
+	//		return
+	//	}
+	//} else if askDownloadTaskReply.Status == 2 {
+	//	// 没有任务
+	//	return
+	//}
 	// 如果收到任务，那么就启动下载，这里下载的任务需要单独地方临时存储
 
 	// 如果下载成功就反馈到云端
