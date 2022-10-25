6a7df95b678bf628dfb7c8606bbbe954dd1903c8
allan716 <525223688@qq.com>
2022-10-25 15:56:37 +0800

    调整回正常启动模式
    Signed-off-by: allan716 <525223688@qq.com>

--- cmd/chinesesubfinder/main.go
+++ cmd/chinesesubfinder/main.go
@@ -125,7 +125,7 @@ func main() {
 		common.SetApiToken("")
 	}
 	// 是否开启开发模式，跳过某些流程
-	settings.Get().SpeedDevMode = true
+	settings.Get().SpeedDevMode = false
 	if settings.Get().SpeedDevMode == true {
 		loggerBase.Infoln("Speed Dev Mode is On")
 		pkg.SetLiteMode(true)
