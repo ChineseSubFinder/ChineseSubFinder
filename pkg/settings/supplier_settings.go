b1a337a92aedfe6060d3de0aca606aecaf1523de
allan716 <525223688@qq.com>
2023-02-22 15:43:39 +0800

    移除过时的设置
    Signed-off-by: allan716 <525223688@qq.com>

--- pkg/settings/supplier_settings.go
+++ pkg/settings/supplier_settings.go
@@ -5,24 +5,18 @@ import (
 )
 
 type SuppliersSettings struct {
-	ChineseSubFinder *OneSupplierSettings `json:"csf"`
-	Xunlei           *OneSupplierSettings `json:"xunlei"`
-	Shooter          *OneSupplierSettings `json:"shooter"`
-	SubHD            *OneSupplierSettings `json:"subhd"`
-	Zimuku           *OneSupplierSettings `json:"zimuku"`
-	Assrt            *OneSupplierSettings `json:"assrt"`
-	A4k              *OneSupplierSettings `json:"a4k"`
+	Xunlei  *OneSupplierSettings `json:"xunlei"`
+	Shooter *OneSupplierSettings `json:"shooter"`
+	Assrt   *OneSupplierSettings `json:"assrt"`
+	A4k     *OneSupplierSettings `json:"a4k"`
 }
 
 func NewSuppliersSettings() *SuppliersSettings {
 	return &SuppliersSettings{
-		ChineseSubFinder: NewOneSupplierSettings(common.SubSiteChineseSubFinder, "haha", "haha", -1),
-		Xunlei:           NewOneSupplierSettings(common.SubSiteXunLei, common.SubXunLeiRootUrlDef, "", -1),
-		Shooter:          NewOneSupplierSettings(common.SubSiteShooter, common.SubShooterRootUrlDef, "", -1),
-		SubHD:            NewOneSupplierSettings(common.SubSiteSubHd, common.SubSubHDRootUrlDef, common.SubSubHDSearchUrl, 20),
-		Zimuku:           NewOneSupplierSettings(common.SubSiteZiMuKu, common.SubZiMuKuRootUrlDef, common.SubZiMuKuSearchFormatUrl, 20),
-		Assrt:            NewOneSupplierSettings(common.SubSiteAssrt, common.SubAssrtRootUrlDef, "", -1),
-		A4k:              NewOneSupplierSettings(common.SubSiteA4K, common.SubA4kRootUrlDef, common.SubA4kSearchUrl, -1),
+		Xunlei:  NewOneSupplierSettings(common.SubSiteXunLei, common.SubXunLeiRootUrlDef, "", -1),
+		Shooter: NewOneSupplierSettings(common.SubSiteShooter, common.SubShooterRootUrlDef, "", -1),
+		Assrt:   NewOneSupplierSettings(common.SubSiteAssrt, common.SubAssrtRootUrlDef, "", -1),
+		A4k:     NewOneSupplierSettings(common.SubSiteA4K, common.SubA4kRootUrlDef, common.SubA4kSearchUrl, -1),
 	}
 }
 
