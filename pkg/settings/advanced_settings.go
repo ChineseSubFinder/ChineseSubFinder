fb32a2c2cdf1004bea0d318d17d9b7323f617b80
skyfish2023 <105966689+skyfish2023@users.noreply.github.com>
2023-10-12 17:37:33 +0800

    Update advanced_settings.go
    自定义扩展名有Bug

--- pkg/settings/advanced_settings.go
+++ pkg/settings/advanced_settings.go
@@ -13,7 +13,7 @@ type AdvancedSettings struct {
 	SubTypePriority            int                `json:"sub_type_priority"`              // 字幕下载的优先级，0 是自动，1 是 srt 优先，2 是 ass/ssa 优先
 	SubNameFormatter           int                `json:"sub_name_formatter"`             // 字幕命名格式(默认不填写或者超出范围，则为 emby 格式)，0，emby 支持的的格式（AAA.chinese(简英,subhd).ass or AAA.chinese(简英,xunlei).default.ass），1常规格式（兼容性更好，AAA.zh.ass or AAA.zh.default.ass）
 	SaveMultiSub               bool               `json:"save_multi_sub"`                 // 保存多个网站的 Top 1 字幕
-	CustomVideoExts            []string           `json:"custom_video_exts""`             // 自定义视频扩展名，是在原有基础上新增。
+	CustomVideoExts            []string           `json:"custom_video_exts"`             // 自定义视频扩展名，是在原有基础上新增。
 	FixTimeLine                bool               `json:"fix_time_line"`                  // 开启校正字幕时间轴，默认 false
 	Topic                      int                `json:"topic"`                          // 搜索结果的时候，返回 Topic N 以内的
 	SuppliersSettings          *SuppliersSettings `json:"suppliers_settings"`             // 每个字幕源的设置
